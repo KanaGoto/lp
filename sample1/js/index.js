3295d36e6d034c2b76dec30ce77bd5df09052a04
Goto Kana <gotokana@Gotos-MacBook-Air.local>
2020-12-29 00:13:36 +0900

    created

---
+++ sample1/js/index.js
@@ -0,0 +1,13 @@
+<script type="text/javascript">
+$(function(){
+  $('.q h3').click(function(){
+    $('.rarrow').show();
+    $('.arrow').hide();
+    $(this).next('.a').slideToggle();
+  });
+    $('.q h3').click(function(){
+    $('.arrow').show();
+    $('.rarrow').hide();
+    });
+});
+</script>
